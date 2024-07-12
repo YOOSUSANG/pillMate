1dd96e592f0b71c66ca09e23e8aef34e57bc618f
YOOSUSANG <dbtntkd456@naver.com>
2024-07-13 01:55:41 +0900

    feat: about controller
    - JWT 테스트를 위한 MainController.java
    - main controller 인 PillController.java
    - JWT 재발급을 위한 ReissueController.java

---
+++ back-end/pillmate_spring/src/main/java/plaform/pillmate_spring/web/controller/PillController.java
@@ -0,0 +1,88 @@
+package plaform.pillmate_spring.web.controller;
+
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.apache.coyote.BadRequestException;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
+import org.springframework.web.bind.annotation.*;
+import plaform.pillmate_spring.domain.entity.History;
+import plaform.pillmate_spring.domain.entity.Member;
+import plaform.pillmate_spring.domain.oauth2.CustomOAuth2User;
+import plaform.pillmate_spring.domain.service.HistoryService;
+import plaform.pillmate_spring.domain.service.MemberService;
+import plaform.pillmate_spring.web.dto.MemberInfoAfterJWTDto;
+
+import java.security.Principal;
+import java.util.*;
+@Slf4j
+@RestController
+@RequiredArgsConstructor
+public class PillController {
+
+
+    private final MemberService memberService;
+    private final HistoryService historyService;
+
+    @GetMapping("/user")
+    public MemberInfoAfterJWTDto getUserInfo(@AuthenticationPrincipal CustomOAuth2User customOAuth2User) throws BadRequestException {
+        String username = customOAuth2User.getUsername();
+        Member member = memberService.findUsername(username);
+        MemberInfoAfterJWTDto memberInfoAfterJWTDto = new MemberInfoAfterJWTDto(member);
+        log.info("user 들어옴");
+        return memberInfoAfterJWTDto;
+    }
+    @PostMapping("/validation/jwt")
+    public ResponseEntity<?> jwtValidation(@AuthenticationPrincipal CustomOAuth2User customOAuth2User) throws BadRequestException {
+        return new ResponseEntity<>(HttpStatus.OK);
+    }
+
+//    @PostMapping("/PillMyProfile")
+//    public Member changeUserValue(@RequestBody Map<String, String> changeData) {
+//        Set<String> keys = changeData.keySet();
+//        List<String> keyList = new ArrayList<>(keys);
+//        String key = keyList.get(0);
+//        String email = keyList.get(1);
+//        String password = changeData.get(email);
+//        if (Objects.equals(key, "img")) {
+//            System.out.println(changeData.get(key));
+//            Long userId = memberService.changeImg(email, password, changeData.get(key));
+//            Member changedImgMember = memberService.findUser(userId);
+//            return changedImgMember;
+//        }
+//        if (Objects.equals(key, "newNickname")) {
+//            Long userId = memberService.changeImg(email, password, changeData.get(key));
+//            Member changedNickMember = memberService.findUser(userId);
+//            return changedNickMember;
+//
+//        }
+//        if (Objects.equals(key, "newPassword")) {
+//            Long userId = memberService.changeImg(email, password, changeData.get(key));
+//            Member changePasswordMember = memberService.findUser(userId);
+//            return changePasswordMember;
+//        }
+//        return null;
+//    }
+    @PostMapping("/pill/detail")
+    public History joinHistory(@RequestBody History history) {
+        History joinHistory = historyService.join(history);
+        return joinHistory;
+    }
+    @PostMapping("/record")
+    public ResponseEntity<String>  removeHistory(@RequestBody Map<String, String> removeData) {
+        String userId = removeData.get("userId");
+        String pillName = removeData.get("pillName");
+        historyService.removeTakePill(Long.valueOf(userId),pillName);
+        return ResponseEntity.ok( "{\"message\": \"ok\"}");
+    }
+
+
+    //get은 PathVariable
+    @GetMapping("/pill/detail/{id}")
+    public List<History> findHistory(@PathVariable Long id) {
+        List<History> takePill = historyService.findTakePill(id);
+        return takePill;
+    }
+
+}
---
+++ back-end/pillmate_spring/src/main/java/plaform/pillmate_spring/web/controller/ReissueController.java
@@ -0,0 +1,122 @@
+package plaform.pillmate_spring.web.controller;
+
+import io.jsonwebtoken.ExpiredJwtException;
+import jakarta.servlet.http.Cookie;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RestController;
+import plaform.pillmate_spring.domain.entity.RedisRefreshToken;
+import plaform.pillmate_spring.domain.jwt.JWTUtil;
+import plaform.pillmate_spring.domain.repository.RedisRefreshTokenRepository;
+
+import java.io.IOException;
+import java.util.Date;
+
+@Slf4j
+@RestController
+@RequiredArgsConstructor
+public class ReissueController {
+
+    private final JWTUtil jwtUtil;
+    private final RedisRefreshTokenRepository redisRefreshTokenRepository;
+
+
+    //access 토큰이 만료될 경우 여기로 보내기
+    @PostMapping("/reissue")
+    public ResponseEntity<?> reissue(HttpServletRequest request, HttpServletResponse response) throws IOException {
+
+        String refresh = null;
+        Cookie[] cookies = request.getCookies();
+
+        //refresh 토큰 얻기
+        for (Cookie cookie : cookies) {
+            if (cookie.getName().equals("refresh")) {
+                refresh = cookie.getValue();
+            }
+        }
+
+        //refresh 토큰이 없으면 토큰이 없다. -> 만료됨
+        if (refresh == null) {
+            return new ResponseEntity<>("refresh token null", HttpStatus.BAD_REQUEST);
+        }
+
+
+        //refresh 토큰이 있는데 만료됐으면 -> redis 사용시 자동 만료
+        try {
+            jwtUtil.isExpired(refresh);
+
+        } catch (ExpiredJwtException e) {
+            // 리프레시 토큰이 만료되면 다시 재 로그인을 권장한다.
+            return new ResponseEntity<>("refresh token expired", HttpStatus.BAD_REQUEST);
+        }
+
+
+        //토큰이 우리 서버에서 보낸 refresh 토큰 인지 확인 (시크릿 키로 검증)
+        String category = jwtUtil.getCategory(refresh);
+
+        if (!category.equals("refresh")) {
+            return new ResponseEntity<>("invaild refresh token", HttpStatus.BAD_REQUEST);
+        }
+
+
+        //DB에 저장된 바로 이전 리프레쉬 토큰인지 확인(바로 이전이 아니면 X)
+//        Boolean isExist = refreshRepository.existsByRefresh(refresh);
+        Boolean isExist = redisRefreshTokenRepository.existsByRefresh(refresh);
+        if (!isExist) {
+            return new ResponseEntity<>("invalid refresh token", HttpStatus.BAD_REQUEST);
+        }
+
+        //리프레시 토큰으로 username과 role 가져오기
+        String username = jwtUtil.getUsername(refresh);
+        String role = jwtUtil.getRole(refresh);
+
+
+        // 액세스 토큰과 리프레시 토큰 재 발급
+        String newAccess = jwtUtil.createJwt("Authorization", username, role, 600000L);
+        String newRefresh = jwtUtil.createJwt("refresh", username, role, 86400000L);
+
+
+        //기존 refresh 토큰 삭제후, 새로운 refresh 토큰 저장
+        RedisRefreshToken deleteRefresh = redisRefreshTokenRepository.findByRefresh(refresh);
+        redisRefreshTokenRepository.delete(deleteRefresh);
+        addRedisRefreshEntity(username, newRefresh);
+
+        response.addCookie(createCookieAccess("Authorization", newAccess));
+        response.addCookie(createCookie("refresh", newRefresh));
+        log.info("refresh 토큰 검증 및 access 토큰 재 발급 완료");
+        return new ResponseEntity<>(HttpStatus.OK);
+    }
+
+    private Cookie createCookie(String key, String value) {
+        Cookie cookie = new Cookie(key, value);
+        int koreaTime = (9 * 60 * 60);
+        cookie.setMaxAge(24 * 60 * 60 + koreaTime);
+        //cookie.setSecure(true);
+        cookie.setPath("/");
+        cookie.setHttpOnly(true); // 자바 스크립트가 해당 쿠기를 읽기만 가능하다. 이 쿠키를 자바스크립트에서 강제로 header에 담아서 보낼 순 없다. 이 쿠키는 HttpOnly라서 --> 대부분 서버는 httpOnly
+        return cookie;
+    }
+
+    private Cookie createCookieAccess(String key, String value) {
+        Cookie cookie = new Cookie(key, value);
+        int koreaTime = (9 * 60 * 60);
+        // 현재 시간에서 10분 후의 시간을 계산
+        cookie.setMaxAge(60 * 10  + koreaTime);
+        //cookie.setSecure(true);
+        cookie.setPath("/");
+        cookie.setHttpOnly(true); // 자바 스크립트가 해당 쿠기를 읽기만 가능하다. 이 쿠키를 자바스크립트에서 강제로 header에 담아서 보낼 순 없다. 이 쿠키는 HttpOnly라서 --> 대부분 서버는 httpOnly
+        return cookie;
+    }
+
+    private void addRedisRefreshEntity(String username, String refresh) {
+        RedisRefreshToken redisRefreshToken = new RedisRefreshToken();
+        redisRefreshToken.setUsername(username);
+        redisRefreshToken.setRefresh(refresh);
+        redisRefreshTokenRepository.save(redisRefreshToken);
+    }
+}
