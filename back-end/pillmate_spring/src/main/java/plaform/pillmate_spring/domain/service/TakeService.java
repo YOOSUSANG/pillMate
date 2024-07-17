c4ea2c8b6566d208fbf5ba9a21e27bc620f36e0d
유수상 <dbtntkd456@naver.com>
2024-07-17 23:45:19 +0900

    feat: TakeRepository.java, TakeService.java - 복용 관련 저장소, 서비스 추가

---
+++ back-end/pillmate_spring/src/main/java/plaform/pillmate_spring/domain/service/TakeService.java
@@ -0,0 +1,49 @@
+package plaform.pillmate_spring.domain.service;
+
+
+import lombok.RequiredArgsConstructor;
+import org.apache.coyote.BadRequestException;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+import plaform.pillmate_spring.domain.entity.Member;
+import plaform.pillmate_spring.domain.entity.Pill;
+import plaform.pillmate_spring.domain.entity.PillOrder;
+import plaform.pillmate_spring.domain.entity.Take;
+import plaform.pillmate_spring.domain.repository.MemberRepository;
+import plaform.pillmate_spring.domain.repository.PillRepository;
+import plaform.pillmate_spring.domain.repository.TakeRepository;
+
+import java.util.List;
+import java.util.Optional;
+
+@Service
+@Transactional(readOnly = true)
+@RequiredArgsConstructor
+public class TakeService {
+
+    private final MemberRepository memberRepository;
+    private final PillRepository pillRepository;
+    private final TakeRepository takeRepository;
+
+    @Transactional
+    public Long pillTakeOne(Long memberId, String name) throws BadRequestException {
+        Optional<Member> findOptionalMember = memberRepository.findById(memberId);
+        Optional<Pill> findOptionalPill = pillRepository.findByName(name);
+        Member member = ValidationService.validationMember(findOptionalMember);
+        Pill pill = ValidationService.validationPill(findOptionalPill);
+        Take pillTake = Take.createTake(member, pill);
+        Take saveTake = takeRepository.save(pillTake);
+        return saveTake.getId();
+    }
+
+    public List<Take> findTakePill(Long memberId) {
+        List<Take> takePill = takeRepository.findByMemberId(memberId);
+        return takePill;
+    }
+
+    @Transactional
+    public void removeTakePill(Long memberId, String name) {
+        takeRepository.deleteByMemberIdAndPillName(memberId,name);
+    }
+
+}
