70966b363819cc2e72bcc298b4d90f5b6c2521d5
유수상 <dbtntkd456@naver.com>
2024-07-27 13:54:12 +0900

    feat: BasicEntity.java
    - 생성일, 수정일과 관련된 공통으로 사용하는 정보 생성

---
+++ back-end/pillmate_spring/src/main/java/plaform/pillmate_spring/domain/entity/BasicEntity.java
@@ -0,0 +1,35 @@
+package plaform.pillmate_spring.domain.entity;
+
+
+import jakarta.persistence.Column;
+import jakarta.persistence.MappedSuperclass;
+import jakarta.persistence.PrePersist;
+import jakarta.persistence.PreUpdate;
+import lombok.Getter;
+import lombok.extern.slf4j.Slf4j;
+
+import java.time.LocalDateTime;
+
+@Slf4j
+@MappedSuperclass
+@Getter
+public abstract class BasicEntity {
+
+    @Column(updatable = false)
+    private LocalDateTime createDate;
+    private LocalDateTime updateDate;
+
+    @PrePersist
+    public void prePersist() {
+        LocalDateTime now = LocalDateTime.now();
+        createDate = now;
+        updateDate = now;
+    }
+
+    @PreUpdate
+    public void preUpdate() {
+        updateDate = LocalDateTime.now();
+    }
+
+
+}
