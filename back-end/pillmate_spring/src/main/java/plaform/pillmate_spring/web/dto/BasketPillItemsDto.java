2dfa43531f7eef5f36c7de5dca3fa0a86fc15499
YOOSUSANG <dbtntkd456@naver.com>
2024-08-04 23:35:23 +0900

    feat: BasketPillItemsDto.java
    - 필드 수정

--- back-end/pillmate_spring/src/main/java/plaform/pillmate_spring/web/dto/BasketPillItemsDto.java
+++ back-end/pillmate_spring/src/main/java/plaform/pillmate_spring/web/dto/BasketPillItemsDto.java
@@ -12,25 +12,38 @@ import plaform.pillmate_spring.domain.entity.BasketPillItem;
 @NoArgsConstructor
 public class BasketPillItemsDto {
 
+    @Schema(description = "장바구니 상품 고유 식별자", defaultValue = "ex) 1, 2, 3, 4")
+    private Long basketItemId;
     @Schema(description = "상품 고유 식별자", defaultValue = "ex) 1, 2, 3, 4")
-    private Long basketPillItemId;
+    private Long id;
     @Schema(description = "상품 이름", defaultValue = "ex) 타이레놀1, 타이레놀2, 타이레놀3")
-    private String name;
+    private String dl_name;
+    @Schema(description = "상품 분류", defaultValue = "ex) 치과구강용약, 기타의 호흡기관용약 ")
+    private String dl_class_no;
 
     @Schema(description = "상품 가격", defaultValue = "ex) 1000, 2000, 3000")
-    private int OrderItemPrice;
+    private int price;
+    @Schema(description = "상품 성분", defaultValue = "플루르비프로펜, 아스피린장용과립 ")
+    private String dl_material;
+    @Schema(description = "상품 성분 EN", defaultValue = "Flurbiprofen, Aspirin Enteric Gr ")
+    private String dl_material_en;
 
     @Schema(description = "상품 수량", defaultValue = "ex) 1, 2, 3")
     private int quantity;
 
+
     @Schema(description = "상품 이미지 URL")
-    private String imgUrl;
+    private String img_key;
 
     public BasketPillItemsDto(BasketPillItem basketPillItem) {
-        this.basketPillItemId = basketPillItem.getId();
-        this.name = basketPillItem.getPill().getName();
-        this.OrderItemPrice = basketPillItem.getPrice();
+        this.basketItemId = basketPillItem.getId();
+        this.id = basketPillItem.getPill().getId();
+        this.dl_name = basketPillItem.getPill().getName();
+        this.dl_class_no = basketPillItem.getPill().getClassNo();
+        this.dl_material = basketPillItem.getPill().getDlMaterial();
+        this.dl_material_en = basketPillItem.getPill().getDlMaterialEn();
+        this.price = basketPillItem.getPrice();
         this.quantity = basketPillItem.getSelectQuantity();
-        this.imgUrl = basketPillItem.getPill().getImgKey();
+        this.img_key = basketPillItem.getPill().getImgKey();
     }
 }
