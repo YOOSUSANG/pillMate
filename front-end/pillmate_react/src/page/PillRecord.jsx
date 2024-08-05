d7b8dc62583ea8a507d119051af17a0c31b059f3
YOOSUSANG <dbtntkd456@naver.com>
2024-08-05 21:15:28 +0900

    React

--- front-end/pillmate_react/src/page/PillRecord.jsx
+++ front-end/pillmate_react/src/page/PillRecord.jsx
@@ -1,50 +1,60 @@
 import PillAllNav from "../component/PillAllNav";
-import React, {useContext, useEffect, useState} from "react";
+import React, { useContext, useEffect, useState } from "react";
 import PillListForm from "../component/PillListForm";
-import {ToastContainer} from "react-toastify";
-import {useNavigate} from "react-router-dom";
-import {pillAllInformation} from "../utils/PillAllInformation";
+import { ToastContainer } from "react-toastify";
+import { useNavigate } from "react-router-dom";
+import { pillAllInformation } from "../utils/PillAllInformation";
 import useAuthorization from "../validaiton/useauthorization";
-
 
 /*
     복용이력 페이지
 */
 
 const pillAllList = pillAllInformation();
+
 const PillRecord = () => {
-    useAuthorization()
-    const [pillRecordList, setPillRecordList] = useState((JSON.parse(localStorage.getItem("takingStatus"))))
-    const navigate = useNavigate()
-    console.log(pillRecordList)
+    useAuthorization();
+    const [pillRecordList, setPillRecordList] = useState([]);
+    const navigate = useNavigate();
+    console.log(pillRecordList);
 
     useEffect(() => {
-
-        const takeData = localStorage.getItem("takingStatus")
-        if(takeData){
-            setPillRecordList(JSON.parse(takeData))
+        const takeData = localStorage.getItem("takingStatus");
+        if (takeData) {
+            setPillRecordList(JSON.parse(takeData));
+        } else {
+            setPillRecordList([]);
         }
     }, []);
+
     const goDetails = () => {
-        navigate("/record/detail")
+        navigate("/record/detail");
+    };
 
-    }
     const goProfile = () => {
-        navigate("/mypage")
-    }
-
+        navigate("/mypage");
+    };
 
     return (
         <div>
             <PillAllNav pillNavState={"pillRecord"} onClick={goProfile}></PillAllNav>
+            
             <div className="take_pillCount">현재 복용 약품: {pillRecordList.length}개</div>
             <div>
-                { pillRecordList && pillRecordList.map((it) => <PillListForm key={it.id} {...it} selectList={it}  buttonType = {"cancellation"} currentState={"pillRecord"} onClick={goDetails}
-
-                ></PillListForm>)}
+                {pillRecordList.map((it) => (
+                    <PillListForm 
+                        key={it.id} 
+                        {...it} 
+                        selectList={it}  
+                        buttonType={"cancellation"} 
+                        currentState={"pillRecord"} 
+                        onClick={goDetails}
+                    />
+                ))}
             </div>
-            <ToastContainer/>
+            <ToastContainer />
         </div>
-    )
-}
+    );
+};
+
 export default PillRecord;
