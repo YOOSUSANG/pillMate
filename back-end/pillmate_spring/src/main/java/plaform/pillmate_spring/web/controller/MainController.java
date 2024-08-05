c3c18db17773d6b3c70eb0d936da5d85df472fe0
YOOSUSANG <dbtntkd456@naver.com>
2024-08-05 21:21:07 +0900

    feat: MainController.java
    - index.html로 redirect

--- back-end/pillmate_spring/src/main/java/plaform/pillmate_spring/web/controller/MainController.java
+++ back-end/pillmate_spring/src/main/java/plaform/pillmate_spring/web/controller/MainController.java
@@ -1,18 +1,30 @@
 package plaform.pillmate_spring.web.controller;
 
 
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
-
+@Slf4j
 @Controller
 public class MainController {
+//
+//
+//    @GetMapping("/")
+//    @ResponseBody
+//    public String mainApi() {
+//        return "main route";
+//    }
 
 
-    @GetMapping("/")
-    @ResponseBody
-    public String mainApi() {
-        return "main route";
+
+    @RequestMapping({"/", "/generalsearch", "/imagesearch", "/imagesearch/detail", "/record",
+            "/generalSearch/list", "/pill/detail", "/record/detail", "/Login",
+            "/mypage", "/mypage/detail", "/map", "/order/success", "/pill_store",
+            "/mypage/order_list", "/mypage/refund_list", "/orders/basket", "/**"})
+    public String redirect() {
+        return "forward:/index.html";
     }
 
 }
